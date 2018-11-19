e29672ef6ab7e24c2a85d284992c625df8f5a77b
isYangJun <isyangjun@126.com>
2018-11-19 11:46:32 +0800

    fix view layer

--- src/main/java/com/yj/foodtracesystem/repository/OperationOrderInfoRepository.java
+++ src/main/java/com/yj/foodtracesystem/repository/OperationOrderInfoRepository.java
@@ -1,13 +1,10 @@
 package com.yj.foodtracesystem.repository;
 
 import com.yj.foodtracesystem.model.OperationOrderInfo;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import sun.management.jdp.JdpPacket;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @Author:yangjun
